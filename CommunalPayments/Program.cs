9cb24b0c93f625f0ef4691625ea0a691e4a572ba
ЛатыповА <@223_9>
2018-05-14 16:26:43 +0300

    Add project files.

---
+++ CommunalPayments/Program.cs
@@ -0,0 +1,29 @@
+﻿/*1.	Написать программу, рассчитывающую сумму коммунальных платежей: есть базовые тарифы на отопление (на 1 м2 площади), 
+ на воду (на 1 чел), на газ (на 1 чел), на текущий ремонт (на 1 м2 площади). Задается метраж помещения, количество проживающих людей, 
+ сезон (осенью и зимой отопление дороже), наличие льгот (ветеран труда– 30 % от его части; ветеран войны- 50% от его части). 
+ Вывести таблицу со столбцами: Вид платежа, Начислено, Льготная скидка, Итого. Посчитать итоговую сумму.*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPayments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculation calculation = new Calculation();
+            calculation.Calculate(100, 5, 5, 100, false, 0);
+
+            Console.WriteLine(calculation[0].ToString());
+
+
+
+        }
+    }
+}
